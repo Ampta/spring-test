abd4c2198dae3caaf1ce5311c38b2bd1ac29ec97
shivam <ampta77000@gmail.com>
2024-12-29 14:30:46 +0530

    Spanish  Printers are added

--- src/main/java/com/ampta/quickstart/config/PrinterConfig.java
+++ src/main/java/com/ampta/quickstart/config/PrinterConfig.java
@@ -8,26 +8,26 @@ import com.ampta.quickstart.service.ColourPrinter;
 import com.ampta.quickstart.service.GreenPrinter;
 import com.ampta.quickstart.service.RedPrinter;
 import com.ampta.quickstart.service.impl.ColourPrinterImpl;
-import com.ampta.quickstart.service.impl.EnglishBluePrinter;
-import com.ampta.quickstart.service.impl.EnglishGreenPrinter;
-import com.ampta.quickstart.service.impl.EnglishRedPrinter;
+import com.ampta.quickstart.service.impl.spanish.SpanishBluePrinter;
+import com.ampta.quickstart.service.impl.spanish.SpanishGreenPrinter;
+import com.ampta.quickstart.service.impl.spanish.SpanishRedPrinter;
 
 @Configuration // It tells spring-boot look for bean declaration
 public class PrinterConfig {
 	
 	@Bean
 	public RedPrinter redPrinter() {
-		return new EnglishRedPrinter();
+		return new SpanishRedPrinter();
 	}
 	
 	@Bean
 	public BluePrinter bluePrinter() {
-		return new EnglishBluePrinter();
+		return new SpanishBluePrinter();
 	}
 	
 	@Bean
 	public GreenPrinter greenPrinter() {
-		return new EnglishGreenPrinter();
+		return new SpanishGreenPrinter();
 	}
 	
 	@Bean
---
+++ src/main/java/com/ampta/quickstart/service/impl/spanish/SpanishGreenPrinter.java
@@ -0,0 +1,11 @@
+package com.ampta.quickstart.service.impl.spanish;
+
+import com.ampta.quickstart.service.GreenPrinter;
+
+public class SpanishGreenPrinter implements GreenPrinter{
+	
+	@Override
+	public String print() {
+		return "verde";
+	}
+}
